0b4e79c05fb97caa17d8095381a66f957a7442dd
Nicola Heald <nicola@notnowlewis.com>
2017-11-24 11:16:13 +0000

    External media: add transient images to WP library
    When working on https://github.com/Automattic/wp-calypso/pull/20008
    I found that after inserting a single image and going directly back
    to the Pexels external media screen, the transient image added
    was still there, making it look like a duplicate image.
    
    This change switches back to the WordPress library screen before
    adding the transient image, so that when going back to the
    external media source, the transient image isn't there, and
    the WP library looks as it should.
    
    This does not change the usage for the user - single images
    are still inserted without any further clicks.
    
    Testing:
    
    Make sure you have a connected Google Photos account.
    Edit a post,  go to the media library, switch to Google Photos
    and add one of your images. The image should automatically
    insert into your post. Add more than one image, or something
    that is not an image, and you should switch to the WordPress
    library screen and stay there once the files are uploaded.

--- client/post-editor/media-modal/test/index.jsx
+++ client/post-editor/media-modal/test/index.jsx
@@ -47,6 +47,8 @@ jest.mock( 'lib/analytics', () => ( {
 jest.mock( 'lib/media/actions', () => ( {
 	delete: () => {},
 	setLibrarySelectedItems: () => {},
+	sourceChanged: () => {},
+	addExternal: () => {},
 } ) );
 jest.mock( 'lib/posts/actions', () => ( {
 	blockSave: () => {},
@@ -75,6 +77,8 @@ describe( 'EditorMediaModal', () => {
 	useSandbox( sandbox => {
 		spy = sandbox.spy();
 		sandbox.stub( mediaActions, 'setLibrarySelectedItems' );
+		sandbox.stub( mediaActions, 'sourceChanged' );
+		sandbox.stub( mediaActions, 'addExternal' );
 		deleteMedia = sandbox.stub( mediaActions, 'delete' );
 		onClose = sandbox.stub();
 	} );
@@ -349,7 +353,7 @@ describe( 'EditorMediaModal', () => {
 			).instance();
 
 			tree.setState( { source: 'external' } );
-			tree.copyExternalAfterLoadingWordPressLibrary = onClose;
+			tree.copyExternal = onClose;
 			tree.confirmSelection();
 
 			// EditorMediaModal will generate transient ID for the media selected
@@ -372,20 +376,24 @@ describe( 'EditorMediaModal', () => {
 					mediaLibrarySelectedItems={ SINGLE_ITEM_MEDIA }
 					view={ ModalViews.DETAIL }
 					setView={ spy }
+					onClose={ onClose }
 				/>
 			).instance();
 
 			tree.setState( { source: 'external' } );
-			tree.copyExternal = onClose;
 			tree.confirmSelection();
 
 			// EditorMediaModal will generate transient ID for the media selected
 			// by using uniqueId, which increments its value within the same session.
 			const transientItems = [
-				Object.assign( {}, SINGLE_ITEM_MEDIA[ 0 ], { ID: 'media-3', transient: true } ),
+				Object.assign( {}, SINGLE_ITEM_MEDIA[ 0 ], {
+					ID: 'media-3',
+					transient: true,
+					mime_type: 'image/jpeg',
+				} ),
 			];
 			process.nextTick( () => {
-				expect( onClose ).to.have.been.calledWith( transientItems, 'external' );
+				expect( onClose ).to.have.been.calledWith( { items: transientItems, type: 'media' } );
 				done();
 			} );
 		} );
@@ -401,7 +409,7 @@ describe( 'EditorMediaModal', () => {
 			).instance();
 
 			tree.setState( { source: 'external' } );
-			tree.copyExternalAfterLoadingWordPressLibrary = onClose;
+			tree.copyExternal = onClose;
 			tree.confirmSelection();
 
 			// EditorMediaModal will generate transient ID for the media selected
