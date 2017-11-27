2b2e54da400b8ff0bb94cb2a6069452a93b73e85
Benoit Person <benoit.person@gmail.com>
2017-11-27 15:00:30 +0100

    Polyfill for all supported browser in Calypso for DNT

--- client/lib/analytics/utils.js
+++ client/lib/analytics/utils.js
@@ -17,7 +17,12 @@ const debug = debugFactory( 'calypso:analytics:utils' );
  * @returns {Boolean} true if Do Not Track is enabled in the user's browser.
  */
 export function doNotTrack() {
-	const result = '1' === navigator.doNotTrack;
+	const result = Boolean(
+		window &&
+			// Internet Explorer 11 uses window.doNotTrack rather than navigator.doNotTrack.
+			// Safari 7.1.3+ uses window.doNotTrack rather than navigator.doNotTrack.
+			( window.doNotTrack === '1' || ( window.navigator && window.navigator.doNotTrack === '1' ) )
+	);
 	debug( `Do Not Track: ${ result }` );
 	return result;
 }
